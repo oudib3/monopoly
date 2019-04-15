7699793012befa18d17d5acef14c67450eede556
oudib3 <doldak71@gmail.com>
2019-04-15 14:40:57 +0200

    users factory password label change

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user, class: User do
         sequence(:email) { |n| "user#{n}@user.pl" }
         sequence(:nickname) { |n| "nickname#{n}" }
-        sequence(:password) {|n| "gowno#{n}"}
-        sequence(:password_confirmation){|n| "gowno#{n}"}
+        sequence(:password) {|n| "pass#{n}"}
+        sequence(:password_confirmation){|n| "pass#{n}"}
     end
 end
